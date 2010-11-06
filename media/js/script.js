98343b40479b907d6d88f0aae63a903a0b55b42a
Joey Baker <joey@byjoeybaker.com>
2010-11-06 16:02:40 -0700

    adding some basic interactions

--- media/js/script.js
+++ media/js/script.js
@@ -1,26 +1,13 @@
-/* Author: 
+/* Author: Joey Baker
 
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function init () {
+	window.location.hash='#intro';
+}
+//onload, we'll call the init function.
+window.onload = init;
+//when we click on an article (a quote), we'll change the hash of the window to it's id
+$('article').click(function(){
+	var id = $(this).attr('id');
+	window.location.hash=id;
+});
