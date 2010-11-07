9326008df7eea1986d5d0cf1dc777607b7f92fbc
aepton <abraham.epton@gmail.com>
2010-11-07 03:40:01 -0800

    Getting tags working

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from google.appengine.ext import db
 from django.utils import simplejson
 import datetime
 import time
+import operator
 
 class QuoteObject(db.Model):
     quote_text = db.StringProperty(multiline=True)
@@ -14,6 +15,7 @@ class QuoteObject(db.Model):
     location = db.GeoPtProperty()
     person_age = db.IntegerProperty()
     photo_url = db.LinkProperty()
+    tags = db.StringListProperty()
 
 
 class MainPage(webapp.RequestHandler):
@@ -22,17 +24,75 @@ class MainPage(webapp.RequestHandler):
         FH = open('static/index.html')
         lines = FH.readlines()
         FH.close()
+        datastore = self.getDataStore()
+        quotelist_delimiter = '||quotelist||'
+        taglist_delimiter = '||taglist||'
+        taglist_all_delimiter = '||taglist_all||'
+        map_delimiter = '||map||'
         for line in lines:
+            if line.find(quotelist_delimiter) != -1:
+                line = line.replace(quotelist_delimiter,
+                                    self.generateQuotelist(datastore))
+            if line.find(taglist_delimiter) != -1:
+                line = line.replace(taglist_delimiter,
+                                    self.generateTaglist(datastore, 5))
+            if line.find(taglist_all_delimiter) != -1:
+                line = line.replace(taglist_all_delimiter,
+                                    self.generateTaglist(datastore, -1))
+            if line.find(map_delimiter) != -1:
+                line = line.replace(map_delimiter, self.generateMap(datastore))
             self.response.out.write(line)
 
-class ViewData(webapp.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
+    def getDataStore(self):
         query = QuoteObject.all()
         results = query.fetch(limit=200)
-        for result in results:
-	        self.response.out.write("<div><img src='img?img_id=%s'></img>" % result.key())
-	        
+        return results
+        
+    def generateQuotelist(self, datastore):
+        item_template = """
+        <article id="quote%d" class="%s">
+            %s
+            <img src="%s" alt="">
+        </article>"""
+        quotelist = ''
+        counter = 0
+        for item in datastore:
+            if not item.quote_text:
+                continue
+            article = item_template % (counter, '', item.quote_text, 
+                                       item.photo_url)
+            quotelist = '%s\n%s' % (quotelist, article)
+            counter += 1
+        return quotelist
+
+    def generateTaglist(self, datastore, limit):
+        item_template = '<li><a href="#%s">%s</a></li>\n'
+        taglist = {}
+        for item in datastore:
+            for tag in item.tags:
+                if tag in taglist:
+                    taglist[tag] += 1
+                else:
+                    taglist[tag] = 0
+        sorted_taglist = sorted(taglist.iteritems(),
+                                key=operator.itemgetter(1))
+        sorted_taglist.reverse()
+        taglist_string = ''
+        if limit == -1:
+            limit = len(sorted_taglist) + 1
+        for tag in sorted_taglist[:limit]:
+            tag_string = item_template % (tag[0], tag[0])
+            all_string = ''
+            if limit == (len(sorted_taglist) + 1):
+                all_string = '%s: %d\n' % (tag[0], tag[1])
+                tag_string = ''
+            taglist_string = '%s%s%s' % (taglist_string, tag_string,
+                                         all_string)
+        return taglist_string
+
+    def generateMap(self, datastore):
+        return ''
+            
 class AddData(webapp.RequestHandler):
     def post(self):
         quote = QuoteObject()
@@ -48,6 +108,40 @@ class AddData(webapp.RequestHandler):
         quote.put()
         self.redirect('/data_form')
 
+class GenerateTagsAttribute(webapp.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.out.write("""
+<html><head><title>Generate Tags</title></head><body>""")
+        query = QuoteObject.all()
+        results = query.fetch(limit=200)
+        banned_words = ['to', 'i', 'the', 'going', 'and', 'my', 'a', 'about',
+                        'when', 'that', 'what', 'get', 'want', 'at', 'this',
+                        'is', "i'm", 'are', 'right', u'i\u2019m', 'me', 'how',
+                        'go', 'so', 'up', 'he', 'way', 'because', 'on', 'come',
+                        'now', 'we', 'be', 'got', 'see', 'where', 'like',
+                        'around', 'had', u'it\u2019s', 'it', 'have', 'just',
+                        'really', 'guess', 'for', 'coming', 'take', 'if',
+                        'was', 'thinking', 'know']
+        for result in results:
+            text = result.quote_text
+            removals = ['.', '(', ')', '"']
+            for removal in removals:
+                text = text.replace(removal, '')
+            words = text.split()
+            taglist = []
+            for word in words:
+                word = word.lower()
+                #word = word.encode('iso-8859-1')
+                if word not in taglist and word not in banned_words:
+                    taglist.append(word)
+            result.tags = taglist
+            self.response.out.write(
+                '<p>Quote: %s<br>Tags: %s</p>' % (result.quote_text,
+                                                  taglist))
+            result.put()
+        self.response.out.write('</body></html>')
+
 class DataForm(webapp.RequestHandler):
     def get(self):
         self.response.out.write("""
@@ -62,17 +156,9 @@ class DataForm(webapp.RequestHandler):
 <div><p>Photo URL<input type="text" name="photo_url"></input></p></div>
 <div><input type="submit" value="Add Quote"></div>
 </form>
+<br><br><a href="http://tenderneeds.appspot.com/generate_tags">Regenerate Tags</a>
 </body>
 </html>""")
-
-class Image (webapp.RequestHandler):
-    def get(self):
-      greeting = db.get(self.request.get("img_id"))
-      if greeting.photo:
-          self.response.headers['Content-Type'] = "image/png"
-          self.response.out.write(greeting.photo)
-      else:
-          self.error(404)
 
 class JSON (webapp.RequestHandler):
     def get(self):
@@ -121,11 +207,10 @@ class JSON (webapp.RequestHandler):
         pass
 
 application = webapp.WSGIApplication(
-                                     [('/view_data', ViewData),
-                                     ('/add_data', AddData),
+                                     [('/add_data', AddData),
                                      ('/data_form', DataForm),
-                                     ('/img', Image),
                                      ('/quotes', JSON),
+                                     ('/generate_tags', GenerateTagsAttribute),
                                      ('/', MainPage)])
 
 def main():
---
+++ uploader.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+uploader.py
+
+Created by Abe Epton on 2010-11-07.
+Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+
+Uploads every entry from a CSV file to an AppEngine app.
+"""
+
+import sys
+import os
+import urllib
+import urllib2
+
+def main():
+    FH = open('trans_to_upload.csv', 'r')
+    text = FH.readlines()
+    FH.close()
+    counter = 0
+    for line in text:
+        quote_locations = []
+        index = 0
+        while index < len(line):
+            if line[index] == '"':
+                quote_locations.append(index)
+            index += 1
+        if len(quote_locations) % 2:
+            print 'Unable to decode line: %s' % line
+            continue
+        index = 0
+        quote_tuples = []
+        while index < len(quote_locations):
+            new_tuple = (quote_locations[index], quote_locations[index + 1])
+            quote_tuples.append(new_tuple)
+            index += 2
+        index = 0
+        while index < len(quote_tuples) - 1:
+            i = quote_tuples[index][0]
+            while i < quote_tuples[index][1]:
+                if line[i] == ',':
+                    line[i] == '|'
+                i += 1
+            index += 1
+        line.replace('"', '')
+        split_line = line.split(',')
+        for split in split_line:
+            split = split.replace('|', ',')
+        if len(split_line) != 7:
+            print 'NOT 7 FIELDS - %d: %s' % (len(split_line), split_line)
+            counter += 1
+            continue
+        entry = {'quote_text': split_line[5], 'quote_text_alt': split_line[6],
+                 'person_name': split_line[1], 'location_lat': split_line[3], 
+                 'location_long': split_line[4], 'person_age': split_line[2]}
+        entry['photo_url'] = 'http://nothing.com'
+        formatted_entry = urllib.urlencode(entry)
+        try:
+            request = urllib2.Request('http://tenderneeds.appspot.com/add_data',
+                                      formatted_entry)
+            connection = urllib2.urlopen(request)
+            print connection.read()
+        except:
+            print entry
+    print 'Need to manually enter %d lines' % d
+
+if __name__ == '__main__':
+    main()
+
